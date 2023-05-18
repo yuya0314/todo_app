f100ee037e239932220c35a75bfdfc88b182bb56
yuya0314 <129703629+yuya0314@users.noreply.github.com>
2023-05-18 05:35:28 +0000

    3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
   def new
   end
+  def create
+    @posts = Post.new(
+      title:params[:title],
+      memo:params[:memo])
+    @posts.save
+    redirect_to("/")
+  end
+
+  def index
+    @posts = Post.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get "hello/index" =>"hello#index"
   get 'posts/new' => "posts#new"
-  get "/" =>"hello#index"
+ 
+  get "/" => "posts#index"
+  post "posts/create" => "posts#create"
   #root "hello#index"
 end
