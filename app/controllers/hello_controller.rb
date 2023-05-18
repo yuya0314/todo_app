ca3b10060d88a683a9e82ec30246e97d4de100ca
yuya0314 <129703629+yuya0314@users.noreply.github.com>
2023-05-18 03:02:06 +0000

    add

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,5 @@
 class HelloController < ApplicationController
   def index
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  get 'posts/new' => "posts#new"
+  get "/" =>"hello#index"
+  #root "hello#index"
 end
